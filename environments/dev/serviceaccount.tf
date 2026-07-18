d95cab382e53fe2c4b57300cf4263ecec608e456
Manigandan Mangundu <jayasriaero14@gmail.com>
2026-07-18 18:56:04 +0530

    modules format change

--- environments/dev/serviceaccount.tf
+++ environments/dev/serviceaccount.tf
@@ -2,5 +2,5 @@ module "github_actions_sa" {
   source               = "../../modules/serviceaccounts/"
   service_account_name = var.github_actions_sa_name
   display_name         = "GitHub Actions Service Account"
-  project = var.project_id
+  project              = var.project_id
 }
